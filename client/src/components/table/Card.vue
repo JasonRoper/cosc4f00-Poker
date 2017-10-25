220b18427a8bfa35c49531aa1bb448617ceedc90
Jason Roper <jkmroper@gmail.com>
2017-10-25 15:11:32 -0400

    outline for cards

---
+++ client/src/components/table/Card.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{ card }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['card']
+}
+</script>
+
+<style>
+
+</style>
+<style scoped src="./assets/css/cards.css"></style>
+
