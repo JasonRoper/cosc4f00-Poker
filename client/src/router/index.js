8e45161de40486000efff3e83db2ea909ec90c76
javon <javon@LAPTOP-PEFEHSBB>
2017-11-15 23:29:05 -0500

    downloaded JS Standard, added css,vue and routes  for Lobby, NextGame and WinLoss

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Table from '@/components/Table'
+import Lobby from '@/components/Lobby'
+import WinLoss from '@/components/WinLoss'
+import NextGame from '@/components/NextGame'
 
 Vue.use(Router)
 
@@ -10,6 +13,21 @@ export default new Router({
       path: '/',
       name: 'Table',
       component: Table
+    },
+    {
+      path: '/NextGame',
+      name: 'NextGame',
+      component: NextGame
+    },
+    {
+      path: '/Lobby',
+      name: 'Lobby',
+      component: Lobby
+    },
+    {
+      path: '/WinLoss',
+      name: 'WinLoss',
+      component: WinLoss
     }
   ]
 })
