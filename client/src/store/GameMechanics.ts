465f0aa2198c5ef63ec0b6f86afc71b927c4b43e
javon <javon@LAPTOP-PEFEHSBB>
2018-01-22 00:02:12 -0500

    GameMechanicsFinished

--- client/src/store/GameMechanics.ts
+++ client/src/store/GameMechanics.ts
@@ -1,18 +1,18 @@
-import { GameService,GameState,GameAction,GameActionType,Player, PlayerEntity } from "@/api/gameservice";
+import { GameAction, GameActionType, GameService, GameState, Player, PlayerEntity } from '@/api/gameservice'
 
 class GameError extends Error {
-  constructor(m: string) {
-      super(m);
+  constructor (m: string) {
+    super(m)
 
-      // Set the prototype explicitly.
-      Object.setPrototypeOf(this, GameError.prototype);
+    // Set the prototype explicitly.
+    Object.setPrototypeOf(this, GameError.prototype)
   }
 
-  actionError() {
-      return "ActionError: " + this.message;
+  public actionError () {
+    return 'ActionError: ' + this.message
   }
-  gameError() {
-    return "GameError: " + this.message;
+  public gameError () {
+    return 'GameError: ' + this.message
   }
 }
 
@@ -21,210 +21,131 @@ class GameMechanics {
   private GAME_SERVICE: GameService
    // Generate the websocket paths used for the given gameID
    // @param gameId - the id of the game
-  
   constructor (gameId: number) {
-    this.GAME_SERVICE = new GameService(gameId);
-    this.GAME_SERVICE.onGameUpdated(this.setGameTransport);
-  }
-
-/**
- * Validates action and sets it as a premove or sends it to a server
- * @param {GameId,Player,GameAction} PlayerEntity
- */
-  public action(PlayerEntity: PlayerEntity)
-  {
-    if(this.isPlayerTurn(PlayerEntity.Player,this.GAME_TRANSPORT.Turn))
-    {
-      if(this.validate(PlayerEntity))
-      {
-        this.send(PlayerEntity.GameAction)
-        return true;
-      }
-      return false;
-    }
-    else{
-      if(this.validate(PlayerEntity))
-      {
-        PlayerEntity.Player.move = PlayerEntity.GameAction
-        return true;
-      }
-      return false;
-    }
-  }
-
-
-  private send(GameAction: GameAction)
-  {
-    this.GAME_SERVICE.sendAction(GameAction);
-  }
-
-
-  /**
-   *  Sets the Game Transport in the Game Mechanics
-   * @param GameState 
-   * 
-   */
-  public setGameTransport(gameTransport: GameState)
-  {
-    //this.GAME_SERVICE.onGameUpdated()
-    if(gameTransport.GameId===this.GAME_TRANSPORT.GameId)
-    {
-      this.GAME_TRANSPORT = gameTransport
-      this.validatePreMove()
-      
-    }else{
-    
-      let error = new GameError("Attempted to Replace GameTransport with a different GameId");
-      if(error instanceof GameError){
-        console.log(error.gameError());
-      }
-    //throw new GAMETRANSPORTEVENT('Attempted to Replace GameTransport with a different GameId');
-    }
+    this.GAME_SERVICE = new GameService(gameId)
+    this.GAME_SERVICE.onGameUpdated(this.setGameTransport)
   }
 
   /**
    *  Gets the Game Transport in the Game Mechanics
-   * @return GameState 
+   * @return GameState
    */
-  public getGameTransport()
-  {
-    return this.GAME_TRANSPORT;
+  public getGameTransport (userId: number, gameId: number) {
+    if (this.removeInfo(userId) && this.validate(userId, gameId)) {
+      return this.GAME_TRANSPORT
+    }
+    return null
   }
-
-  public validatePreMove()
-  {
-    var players = this.GAME_TRANSPORT.MultiplePlayers
-
-    for(let user of players)
-    {
-      if(!(this.validateAction(user,user.move,this.GAME_TRANSPORT.HasBet)))
-      {
-        user.move.type=null;
-        user.move.bet=0;
+  public removeInfo (userId: number) {
+    for (const index of this.GAME_TRANSPORT.MultiplePlayers) {
+      if (index.id === userId) {
+        if (this.GAME_TRANSPORT.HasBet) {
+          index.action[0].valid = false
+        } else {
+          index.action[1].valid = false
+        }
+      } else {
+        index.card1 = null
+        index.card2 = null
+        index.move = null
+        index.id = null
       }
     }
+    this.GAME_TRANSPORT.Deck = null
   }
 
-
-
- 
-  
-  public validate(PlayerEntity: PlayerEntity)
-  {
-    let valid = true;
-
-    valid = this.validateAction(PlayerEntity.Player,PlayerEntity.GameAction,this.GAME_TRANSPORT.HasBet)
-    if(!valid){return false}
-    valid = this.isPlayerTurn(PlayerEntity.Player,this.GAME_TRANSPORT.Turn)
-    if(!valid){return false}
+  public validate (userId: number, gameId: number) {
+    let valid = true
+    valid = this.isPlayerTurn(userId)
+    if (!valid) { return false }
     valid = this.isGameRunning()
-    if(!valid){return false}
-    valid = this.isPlayerInGame(PlayerEntity);
-    if(!valid){return false}
-    valid = this.actionByPlayer(PlayerEntity)
-    if(!valid){return false}
-
+    if (!valid) { return false }
+    valid = this.isPlayerInGame(userId, gameId)
+    if (!valid) { return false }
     return valid
   }
 
-
-
-
+  public validateMove (userId: number, move: GameAction) {
+    for (const index of this.GAME_TRANSPORT.MultiplePlayers) {
+      if (index.id === userId) {
+        if (move.type === GameActionType.FOLD) {
+          return true
+        }
+        if (move.valid) {
+          return true
+        }
+      }
+    }
+    return false
+  }
+/**
+ * Validates action and sets it as a premove or sends it to a server
+ * @param {GameId,Player,GameAction} PlayerEntity
+ */
+  public action (userId: number, gameId: number, move: GameAction) {
+    if (this.validate(userId,gameId)) {
+      if (this.validateMove(userId,move)) {
+        this.send(move)
+        return true
+      }
+    }
+    return false
+  }
+  /**
+   *  Sets the Game Transport in the Game Mechanics
+   * @param GameState
+   */
+  public setGameTransport (gameTransport: GameState) {
+    // this.GAME_SERVICE.onGameUpdated()
+    if (gameTransport.GameId === this.GAME_TRANSPORT.GameId) {
+      this.GAME_TRANSPORT = gameTransport
+      this.validatePreMove()
+    } else {
+      const error = new GameError('Attempted to Replace GameTransport with a different GameId')
+      if (error instanceof GameError) {
+        console.log(error.gameError())
+      }
+    // throw new GAMETRANSPORTEVENT('Attempted to Replace GameTransport with a different GameId');
+    }
+  }
 
   /**
    *  Sets the Game Transport in the Game Mechanics
-   * @param GameState 
-   * 
+   * @param GameState
    */
-  public validateAction(Player: Player,Action: GameAction,HasBet: boolean)
-  {
 
-    switch(Action.type)
-    {
-      case 'FOLD':
-        return true;
-      case 'CALL':
-      case 'RAISE':
-        if(HasBet)
-        {
-          return true;
-        }
-        else{
-          return false;
-        }
-      case 'CHECK':
-      case 'BET':
-        if(HasBet)
-        {
-          return false
-        }
-        else{
+  // is it the players turn?
+  public isPlayerTurn (userId: number) {
+    const turn = this.GAME_TRANSPORT.Turn
+    for (let i = 0;i < this.GAME_TRANSPORT.MultiplePlayers.length;i++) {
+      if (userId === this.GAME_TRANSPORT.MultiplePlayers[i].id) {
+        if (i === turn) {
           return true
         }
-      default:
-      let error = new GameError("Got a different Action Type than expected");
-      if(error instanceof GameError){
-        console.log(error.actionError());
       }
-      
-    }
-
-return false;
-  }
- 
-  //is it the players turn?
-  public isPlayerTurn(Player: Player, Turn: number)
-  {
-    if(Player.id===Turn)
-    {
-      return true
     }
     return false
   }
 
-  public isGameRunning()
-  {
-    return this.GAME_SERVICE.connected();
+  public isGameRunning () {
+    return this.GAME_SERVICE.connected()
   }
-
-
-
-
-
-  //is this particular game running?
-  public isPlayerInGame(PlayerEntity: PlayerEntity)
-  {
-    //if this is not the game you are supposed to be at it returns false
-    if(!(this.GAME_TRANSPORT.GameId===PlayerEntity.GameId))
-    {
-      return false
-    }    
-    var players = this.GAME_TRANSPORT.MultiplePlayers
-
-      for(var i=0;i<players.length;i++)
-      {
-        if(players[i].id==PlayerEntity.Player.id)
-        {
+  // is this particular game running?
+  public isPlayerInGame (userId: number, gameId: number) {
+    // if this is not the game you are supposed to be at it returns false
+    if ((this.GAME_TRANSPORT.GameId === gameId)) {
+      const players = this.GAME_TRANSPORT.MultiplePlayers
+      for (const index of this.GAME_TRANSPORT.MultiplePlayers) {
+        if (index.id === userId) {
           return true
         }
       }
+    }
     return false
   }
-
-
-
-
-  //can this game accecpt action from this player
-  public actionByPlayer(PlayerEntity: PlayerEntity)
-  {
-    var players = this.GAME_TRANSPORT.MultiplePlayers
-    
-    //if it is your turn and the Game you are apart of is the current one
-      if(this.GAME_TRANSPORT.Turn===PlayerEntity.Player.id && PlayerEntity.GameId=== this.GAME_TRANSPORT.GameId)
-      {
-        return true
-      }
-    return false
+  // can this game accecpt action from this player
+  private send (GameAction: GameAction) {
+    this.GAME_SERVICE.sendAction(GameAction)
   }
 
 }
