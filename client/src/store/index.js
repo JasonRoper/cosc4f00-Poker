285986a435cf7431a2bfc141cb053086ae16050c
Ashley Morley <ah10se@brocku.ca>
2018-01-25 17:58:39 -0500

    Fixed Global stores

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import router from '@/router'
 Vue.use(Vuex)
+
 export const store = new Vuex.Store({
   state: {name: '', id: ''},
   mutations: {
@@ -14,6 +16,19 @@ export const store = new Vuex.Store({
   getters: {
     user (state) {
       return state.name
+    }
+  },
+  actions: {
+    storeName (context, name) {
+      console.log('clicked')
+      if (name.length === 0) {
+        return
+      } else
+      if (name.length > 0) {
+        context.commit('setPlayer', name)
+        router.push('Lobby')
+      }
+      console.log(this.state.name)
     }
   }
 })
