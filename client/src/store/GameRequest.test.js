964ee87cefb484d3674c0ebdbc7d1e6d4de4f8cf
javon <javon@LAPTOP-PEFEHSBB>
2018-03-07 16:14:13 -0500

    Commented GameRequest test

--- client/src/store/GameRequest.test.js
+++ client/src/store/GameRequest.test.js
@@ -1,4 +1,6 @@
+/*
 it('subtracts 5 - 1 to equal 4 in TypeScript', () => {
     const sub = require('./GameRequest')
-    expect(sub(5, 1)).toBe(4);
-  });
+    expect(sub(5, 1)).toBe(4)
+  })
+  */
