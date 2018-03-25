1810aba3caa167c51922a05d28c06b6982bfd8c2
Jason <jkmroper@gmail.com>
2018-03-25 15:22:38 -0400

    improved implementation of websocket (allowing multiple connections, much lower boilerplate)

--- api/src/test/java/com/pokerface/pokerapi/game/TestWebsocket.java
+++ api/src/test/java/com/pokerface/pokerapi/game/TestWebsocket.java
@@ -14,6 +14,7 @@ import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.simp.stomp.*;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.socket.WebSocketHttpHeaders;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
@@ -47,59 +48,103 @@ public class TestWebsocket {
         this.URL = "ws://localhost:" + port + "/live";
     }
 
-    public StompSession getStompSession(String username, String password) throws InterruptedException, ExecutionException, TimeoutException, KeyManagementException, NoSuchAlgorithmException {
-
-        StandardWebSocketClient socket = new StandardWebSocketClient();
-
-        WebSocketStompClient stompClient = new WebSocketStompClient(socket);
-        stompClient.setMessageConverter(new MappingJackson2MessageConverter());
-
-
-        WebSocketHttpHeaders headers = new WebSocketHttpHeaders();
-        if (username != null) {
-            restTemplate = restTemplate.withBasicAuth(username, password);
-
-            ResponseEntity<String> response = restTemplate.getForEntity("/api/v1/users/login", String.class);
-            String cookies = response.getHeaders().getFirst(HttpHeaders.SET_COOKIE);
-            headers.add(HttpHeaders.COOKIE, cookies.split(";")[0]);
-        }
-
-        return stompClient.connect(this.URL, headers, new StompSessionHandlerAdapter() {
-            @Override
-            public void handleException(StompSession session, StompCommand command, StompHeaders headers, byte[] payload, Throwable exception) {
-                String str = null;
-                try {
-                    str = new String(payload, "UTF-8");
-                } catch (UnsupportedEncodingException e) {
-                    System.out.println("cannot parse payload, invalid UTF-8");
-                }
-                System.out.println("Exception occured on command: " + command + " with headers: " + headers + " on payload: " + str + " with exception: " + exception);
-                if (future != null) {
-                    future.cancel(true);
-                }
-
-            }
-        }).get();
-    }
-
-
     @Test
     public void testTestEndpoint() throws Exception {
-        StompSession session = getStompSession("admin", "admin");
-        future = new CompletableFuture<GameInfoTransport>();
-        session.subscribe("/messages/game", new StompFrameHandler() {
-            @Override
-            public Type getPayloadType(StompHeaders headers) {
-                return GameInfoTransport.class;
-            }
-
-            @Override
-            public void handleFrame(StompHeaders headers, Object payload) {
-                future.complete((GameInfoTransport) payload);
-            }
-        });
-
-        session.send("/app/test", 1);
+        WebsocketSession socket = new WebsocketSession(this.URL, "admin", "admin");
+        CompletableFuture future = socket.subscribe("/messages/game", GameInfoTransport.class);
+        socket.send("/app/test", 1);
+        
         Assert.assertEquals(1, ((GameInfoTransport) future.get()).getGameId());
     }
+
+    public class MyStompFrameHandler implements StompFrameHandler {
+        private Type type;
+        private CompletableFuture future;
+
+        public MyStompFrameHandler(CompletableFuture future, Type type) {
+            this.type = type;
+            this.future = future;
+        }
+
+        @Override
+        public Type getPayloadType(StompHeaders headers) {
+            return type;
+        }
+
+        @Override
+        public void handleFrame(StompHeaders headers, Object payload) {
+            future.complete(payload);
+        }
+    }
+
+    public class WebsocketSession {
+
+        private CompletableFuture future;
+        private StompSession session;
+
+        /**
+         * Create an unauthenticated websocket connection
+         */
+        public WebsocketSession(String path) throws ExecutionException, InterruptedException {
+            this.connect(path, null);
+        }
+
+        public WebsocketSession(String path, String username, String password) throws ExecutionException, InterruptedException {
+            TestRestTemplate restTemplate = TestWebsocket.this.restTemplate.withBasicAuth(username, password);
+            ResponseEntity<String> response = restTemplate.getForEntity("/api/v1/users/login", String.class);
+            String authCookie = response.getHeaders().getFirst(HttpHeaders.SET_COOKIE).split(";")[0];
+            this.connect(path, authCookie);
+        }
+
+        private void connect(String path, String authCookie) throws ExecutionException, InterruptedException {
+            StandardWebSocketClient socket = new StandardWebSocketClient();
+
+            WebSocketStompClient stompClient = new WebSocketStompClient(socket);
+            stompClient.setMessageConverter(new MappingJackson2MessageConverter());
+
+
+            WebSocketHttpHeaders headers = new WebSocketHttpHeaders();
+            if (authCookie!= null) {
+                headers.add(HttpHeaders.COOKIE, authCookie);
+            }
+
+
+            this.session = stompClient.connect(path, headers, new StompSessionHandlerAdapter() {
+                @Override
+                public void handleException(StompSession session, StompCommand command, StompHeaders headers, byte[] payload, Throwable exception) {
+                    String str = null;
+                    try {
+                        str = new String(payload, "UTF-8");
+                    } catch (UnsupportedEncodingException e) {
+                        System.out.println("cannot parse payload, invalid UTF-8");
+                    }
+                    System.out.println("Exception occured on command: " + command + " with headers: " + headers + " on payload: " + str + " with exception: " + exception);
+                    if (future != null) {
+                        future.cancel(true);
+                    }
+
+                }
+            }).get();
+        }
+
+        public CompletableFuture makeFuture(Type type) {
+            this.future = new CompletableFuture<Type>();
+            return this.future;
+        }
+
+        public StompSession getSession() {
+            return session;
+        }
+
+        public CompletableFuture subscribe(String path, Type type) {
+            CompletableFuture future = makeFuture(type);
+            session.subscribe(path, new MyStompFrameHandler(future, type));
+            return future;
+        }
+
+        public void send(String path, Object payload) {
+            session.send(path, payload);
+        }
+    }
+
 }
